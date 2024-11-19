d6f57ea664bd9b7c550d897beadc63450e0bb771
Igor Goncharov <ihar@hancharou.com>
2024-11-19 00:49:29 -0500

    Upgrading to node20

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const core = require('@actions/core');
 const dayjs = require('dayjs');
-const dayjsPluginUTC = require('dayjs/plugin/utc')
+const dayjsPluginUTC = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
 
-dayjs.extend(dayjsPluginUTC)
+dayjs.extend(dayjsPluginUTC);
+dayjs.extend(timezone);
 
 async function main() {
 	try {
@@ -11,8 +13,16 @@ async function main() {
 		console.log('time zone: ', timezone)
 		console.log('time format: ', formatStr)
 
-		const str = dayjs().utcOffset(timezone).format(formatStr)
-		console.log("time formatStr: ", str)
+		let str;
+		if (Number.isInteger(timezone)) {
+			str = dayjs().utcOffset(timezone).format(formatStr);
+			console.log("Via offset: ", str)
+		}
+		else{
+			str = dayjs().tz(timezone).format(formatStr);
+			console.log("Via tz: ", str)
+		}
+
 
 		core.setOutput("time", str);
 
